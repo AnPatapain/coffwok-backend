2426d22d01286dbb52ce4364d9812d916884a10a
anpatapain <nkalk192002@gmail.com>
2023-07-05 11:46:22 +0200

    deploy: reponsive for chat feat

--- src/main/java/com/anpatapain/coffwok/chat/config/WebSocketConfig.java
+++ src/main/java/com/anpatapain/coffwok/chat/config/WebSocketConfig.java
@@ -30,7 +30,6 @@ import java.util.Map;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //    private String client_app_url = "http://localhost:5173";
     private String client_app_url = "https://www.coffwok.com";
-
     private Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
 
     private JwtUtils jwtUtils;
